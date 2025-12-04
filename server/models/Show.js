ac4afdd9aef5d30644a76ef959ca6874a95e05b7
Khushiya <khushiyabhoge@gmail.com>
2025-12-04 11:02:05 +0530

    refactor: clean up Show model and update Vercel configuration for API routing

--- server/models/Show.js
+++ server/models/Show.js
@@ -1,17 +1,3 @@
-// import mongoose from "mongoose";
-
-// const showSchema = new mongoose.Schema({
-//     movie: {type: String, required: true, ref: 'Movie'},
-//     showDateTime: {type: Date, required: true},
-//     showPrice : {type: Number, required: true},
-//     occupiedSeats: {type: Object, default: {}},
-// }, {minimize: false}
-// );
-
-// const Show = mongoose.model('Show', showSchema);
-
-// export default Show;
-
 import mongoose from "mongoose";
 
 const showSchema = new mongoose.Schema({
@@ -25,3 +11,4 @@ const showSchema = new mongoose.Schema({
 const Show = mongoose.model('Show', showSchema);
 
 export default Show;
+
